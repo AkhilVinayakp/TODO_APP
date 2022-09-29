fc7d57199290da61b5009a66b9c14d7a41a8a445
Akhil Vinayak P <nirvanaosborn@gmail.com>
2022-09-29 10:34:40 +0530

    refactor: code update

--- user/routes.py
+++ user/routes.py
@@ -3,12 +3,12 @@
 Creating using blueprints
 
 '''
-from flask import Blueprint, render_template, redirect
+from flask import Blueprint, render_template, redirect, session
 from user.models import User
 
 client_actions = Blueprint('client_actions', '__name__', static_folder='../static', template_folder='../templates')
 
-@client_actions.route('/dashboard', methods=['POST'])
+@client_actions.route('/dashboard', methods=['POST', 'GET'])
 def dashborad():
     user = User()
     status_code = user.sign_up()
@@ -16,3 +16,4 @@ def dashborad():
         redirect('/home.html')
     name = user.get_user_name
     return render_template("dashboard.html", user_name = name)
+
